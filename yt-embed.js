745045081d6a9ca6a4d9d76827ecd099d4e9a65e
Julio <julioarhernandez@users.noreply.github.com>
2025-11-10 08:41:56 -0500

    Change YouTube embed to unmute video by default

--- yt-embed.js
+++ yt-embed.js
@@ -2,5 +2,6 @@ const params = new URLSearchParams(location.search);
 const id = params.get("v");
 if (id) {
   const iframe = document.getElementById("ytProxy");
-  iframe.src = `https://www.youtube.com/embed/${id}?autoplay=1&mute=1&controls=0&modestbranding=1&disablekb=1`;
+  iframe.src = `https://www.youtube.com/embed/${id}?autoplay=1&mute=0&controls=0&modestbranding=1&disablekb=1`;
 }
+
