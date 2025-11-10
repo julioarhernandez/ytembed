f48404cc0095b850e1257528367ecf0ff0357f56
Julio <julioarhernandez@users.noreply.github.com>
2025-11-10 09:16:33 -0500

    Enable controls in YouTube embed iframe

--- yt-embed.js
+++ yt-embed.js
@@ -2,6 +2,7 @@ const params = new URLSearchParams(location.search);
 const id = params.get("v");
 if (id) {
   const iframe = document.getElementById("ytProxy");
-  iframe.src = `https://www.youtube.com/embed/${id}?autoplay=1&mute=0&controls=0&modestbranding=1&disablekb=1`;
+  iframe.src = `https://www.youtube.com/embed/${id}?autoplay=1&mute=0&controls=1&modestbranding=1&disablekb=1`;
 }
+
 
